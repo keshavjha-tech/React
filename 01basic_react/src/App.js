65c1ec901654f38c356c9a5ce90fee4d28460913
keshavjha205 <keshavjha1081@gmail.com>
2025-01-15 13:31:18 +0530

    React start

---
+++ 01basic_react/src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+ <div><h1>chai aur react | Keshav Jha</h1>
+  
+  <h2>lec 02- create react project </h2>
+  <h2> select the folder and execute command 
+    <h3> npm run start</h3>
+  </h2>
+  
+ </div>
+  
+  );
+}
+
+export default App;
---
+++ 01vite_react/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+
+  return (
+   
+    <h1>chai aur react with vite |KJ</h1>
+  )
+}
+
+export default App
