1d4e73e6d2afa630f1a119914f5fb4a48984fccc
keshavjha205 <keshavjha1081@gmail.com>
2025-01-16 11:24:20 +0530

    React

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,12 +1,17 @@
 
+import Chai from "./chai"
 
 function App() {
 
-
+//app jsx me ek hi element return kr skte hai toh aap div me wrap kar skte ho 
   return (
-   
-    <h1>chai aur react with vite |KJ</h1>
+    <> 
+    <h1>Chai aur react</h1>
+   <Chai/>
+   </>
+
   )
 }
 
 export default App
+
