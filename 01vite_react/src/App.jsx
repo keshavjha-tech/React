32ab9830d21e9d3b3234120c0fd420d0fb37488b
keshavjha205 <keshavjha1081@gmail.com>
2025-01-17 19:39:52 +0530

    React

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -4,6 +4,9 @@ import Chai from "./chai"
 function App() {
 
 //app jsx me ek hi element return kr skte hai toh aap div me wrap kar skte ho 
+ //yaha react hume bs ek hi component return krne deta h 
+    //isliye hum fragment k use krte h jo single component ki trh behave krta h pr uske andr kitne bhi component likh skte hai
+    //ya div tag ka bhi use kr skte h but prefer fragment tag(<></>)
   return (
     <> 
     <h1>Chai aur react</h1>
