98028e03698e2ddfe20ce5275b9bb3758b1037f2
keshavjha205 <keshavjha1081@gmail.com>
2025-01-25 20:48:04 +0530

    bgChanger

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColour] = useState("Olive")
+
+  return (
+   <div className="w-full">
+
+   </div>
+  )
+}
+
+export default App
