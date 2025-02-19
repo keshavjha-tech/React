f34a60018452c92aa19080bc0ba17981a568e2f3
keshavjha205 <keshavjha1081@gmail.com>
2025-02-19 13:04:48 +0530

    bgchanger

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-3xl">  
           <button
           onClick={()=>setColour("red")} //onclick accept krta hai function toh ek callback chaiye hoga
             className="outline-none px-4 py-2 rounded-full shadow-xl"
