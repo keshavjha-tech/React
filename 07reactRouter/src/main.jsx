eb50339cb4bade1d3b4a16a4d57e6617b665a89f
keshavjha205 <keshavjha1081@gmail.com>
2025-03-20 21:02:48 +0530

    React Router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -36,6 +36,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />}>
+      <Route path='keshav' />
       </Route>
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
