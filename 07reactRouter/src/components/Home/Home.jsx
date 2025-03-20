4df06e436c81bf211f1d4170d7fccb7e0317300a
keshavjha205 <keshavjha1081@gmail.com>
2025-03-20 21:01:45 +0530

    React Router

--- 07reactRouter/src/components/Home/Home.jsx
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -32,7 +32,7 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img   className="w-96 transform scale-x-[-1]" src="https://png.pngtree.com/png-clipart/20230916/original/pngtree-sticker-with-a-person-sitting-in-a-chair-on-a-beach-png-image_12251891.png" alt="image1" />
                 </div>
             </aside>
 
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 //  const router = createBrowserRouter([
 //    {
@@ -32,17 +33,21 @@ import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}> 
-     <Route path='' element={<Home/>} /> 
-     <Route path='about' element={<About/>} /> 
-     <Route path='contact' element={<Contact/>} /> 
-     <Route path='user/:userid' element={<User/>} /> 
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />}>
+      </Route>
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route
+        loader = {githubInfoLoader}
+        path='github' element={<Github />} />
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
